dded64ced93f7a3676afdea04ef9478dd48c64a8
Omer Blau <omerblau@gmail.com>
2025-06-25 22:25:23 +0300

    progres

--- frontend/static/script.js
+++
@@ -1,8 +0,0 @@
-fetch("http://backend:5000/api/quote")
-    .then((res) => res.json())
-    .then((data) => {
-        document.getElementById("quote").innerText = data.quote;
-    })
-    .catch((err) => {
-        document.getElementById("quote").innerText = "Error fetching quote.";
-    });
